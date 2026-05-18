84dc8bb78ff9f6f1001cf2da5e49d2e14739b3cd
Dang <gnadegreneyugn@gmail.com>
2026-05-18 18:31:25 +0700

    init terraform pipeline

---
+++ iam_roles.tf
@@ -0,0 +1,33 @@
+data "aws_iam_role" "github_role" {
+  name = "GithubActionsWorkflowRole" 
+}
+
+resource "aws_iam_policy" "vpc_minimal_policy" {
+  name        = "github-actions-vpc-minimal"
+  description = "Quyen toi thieu de tao VPC tu dong"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:DescribeVpcs",
+          "ec2:ModifyVpcAttribute",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:DescribeSubnets",
+          "ec2:CreateTags"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_minimal" {
+  role       = data.aws_iam_role.github_role.name
+  policy_arn = aws_iam_policy.vpc_minimal_policy.arn
+}
