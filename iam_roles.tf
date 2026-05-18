a8a11fe6c157a80935b1e5d799d5604540dc6654
Dang <gnadegreneyugn@gmail.com>
2026-05-18 20:35:04 +0700

    fix: iam_role

--- iam_roles.tf
+++ iam_roles.tf
@@ -19,6 +19,18 @@ resource "aws_iam_role_policy" "github_actions_iam_policy" {
         Action = "iam:GetRole"
         # Terraform tự điền Account ID ở đây khi chạy 
         Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GithubActionsWorkflowRole"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:CreatePolicy",
+          "iam:DeletePolicy",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions"
+        ]
+
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/github-actions-vpc-minimal"
       }
     ]
   })
@@ -45,18 +57,6 @@ resource "aws_iam_policy" "vpc_minimal_policy" {
           "ec2:CreateTags"
         ]
         Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${data.aws_iam_role.github_role.name}"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "iam:CreatePolicy",
-          "iam:DeletePolicy",
-          "iam:GetPolicy",
-          "iam:GetPolicyVersion",
-          "iam:ListPolicyVersions"
-        ]
-
-        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/github-actions-vpc-minimal"
       }
     ]
   })
