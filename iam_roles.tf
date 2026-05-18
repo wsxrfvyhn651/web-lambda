85ab7f5ec5ccf00e9158e78aece93273560bf326
Dang <gnadegreneyugn@gmail.com>
2026-05-18 19:47:11 +0700

    fix: iam_role

--- iam_roles.tf
+++ iam_roles.tf
@@ -2,6 +2,28 @@ data "aws_iam_role" "github_role" {
   name = "GithubActionsWorkflowRole"
 }
 
+
+# Lấy thông tin tài khoản AWS hiện tại một cách tự động
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role_policy" "github_actions_iam_policy" {
+  name = "github-actions-iam-permissions"
+  role = data.aws_iam_role.github_role.id
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "iam:GetRole"
+        # Terraform tự điền Account ID ở đây khi chạy 
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GithubActionsWorkflowRole"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "vpc_minimal_policy" {
   name        = "github-actions-vpc-minimal"
   description = "Quyen toi thieu de tao VPC tu dong"
@@ -31,25 +53,4 @@ resource "aws_iam_policy" "vpc_minimal_policy" {
 resource "aws_iam_role_policy_attachment" "attach_minimal" {
   role       = data.aws_iam_role.github_role.name
   policy_arn = aws_iam_policy.vpc_minimal_policy.arn
-}
-
-# Lấy thông tin tài khoản AWS hiện tại một cách tự động
-data "aws_caller_identity" "current" {}
-
-resource "aws_iam_role_policy" "github_actions_iam_policy" {
-  name = "github-actions-iam-permissions"
-  role = data.aws_iam_role.github_role.id
-
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "iam:GetRole"
-        # Terraform tự điền Account ID ở đây khi chạy 
-        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GithubActionsWorkflowRole"
-      }
-    ]
-  })
 }
