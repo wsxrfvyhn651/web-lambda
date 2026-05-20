e4faecb36ff39732e60b39846b50d30656a4f07c
Dang <gnadegreneyugn@gmail.com>
2026-05-20 12:06:23 +0700

    create lambda function, dynamodb, vpc and configure the netwwork

--- iam_roles.tf
+++ iam_roles.tf
@@ -73,7 +73,7 @@ data "aws_iam_policy_document" "github_access" {
     actions = [
       "lambda:CreateFunction", "lambda:DeleteFunction", "lambda:GetFunction",
       "lambda:UpdateFunctionCode", "lambda:UpdateFunctionConfiguration",
-      "iam:PassRole", "apigateway:*" # Cần thêm quyền APIGW
+      "iam:PassRole", "apigateway:*", "ec2:DescribeManagedPrefixLists" # Cần thêm quyền APIGW
     ]
     resources = ["*"]
   }
